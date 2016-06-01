27582efe97b7b698fb5a81ecea78ccc1f5daceee
Valneja <vs7315@student.uni-lj.si>
2016-06-01 21:38:15 +0000

    delujoč ehr

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -40,3 +40,101 @@ function generirajPodatke(stPacienta) {
 
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+$(document).ready(function() {
+
+  /**
+   * Napolni testne vrednosti (ime, priimek in datum rojstva) pri kreiranju
+   * EHR zapisa za novega bolnika, ko uporabnik izbere vrednost iz
+   * padajočega menuja (npr. Regina Phalange).
+   */
+  $('#preberiPredlogoBolnika').change(function() {
+    $("#kreirajSporocilo").html("");
+    var podatki = $(this).val().split(",");
+    $("#kreirajIme").val(podatki[0]);
+    $("#kreirajPriimek").val(podatki[1]);
+    $("#kreirajDatumRojstva").val(podatki[2]);
+    $("#preberiEHRid").val(podatki[3]);
+  });
+  
+  $('#preberiObstojeciEHR').change(function() {
+		$("#preberiSporocilo").html("");
+		$("#preberiEHRid").val($(this).val());
+	});
+
+});
+
+
+function kreirajEHRzaBolnika() {
+ dodajOsebe(1);
+ dodajOsebe(2);
+ dodajOsebe(3);
+ var select = document.getElementById("preberiPredlogoBolnika");
+ var length = select.options.length;
+
+
+
+}
+function dodajOsebe(stevilka){
+        var oseba;
+        var podatki;
+        
+        if(stevilka == 1){
+            oseba=["Regina","Phalange","1983-10-26T15:38"];
+            podatki=[["2015-11-11T11:44Z",170,60,36,122,66],["2015-11-12T11:44Z",170,59,36,120,60],["2015-11-13T11:44Z",170,61,36,130,70]];
+        }
+        else if(stevilka == 2){
+            oseba=["Ken","Adams","1982-2-6T11:10"];
+            podatki=[["2015-11-11T11:44Z",183,90,37,140,80],["2015-11-12T11:44Z",183,88,37,138,84],["2015-11-13T11:44Z",183,87,37,133,77]];
+        }
+        else if(stevilka == 3){
+            oseba=["Chanandler","Bong","1981-11-21T16:44"];
+            podatki=[["2015-11-11T11:44Z",187,86,36,111,71],["2015-11-12T11:44Z",187,85,36,110,68],["2015-11-13T11:44Z",187,83,37,114,67]];
+        }
+  
+        
+    	sessionId = getSessionId();
+    
+    	var ime = oseba[0];
+    	var priimek = oseba[1];
+    	var datumRojstva = oseba[2];
+    
+    	if (!ime || !priimek || !datumRojstva || ime.trim().length == 0 ||
+          priimek.trim().length == 0 || datumRojstva.trim().length == 0) {
+    		$("#kreirajSporocilo").html("<span class='obvestilo label " +
+          "label-warning fade-in'>Prosim vnesite zahtevane podatke!</span>");
+    	} else {
+    		$.ajaxSetup({
+    		    headers: {"Ehr-Session": sessionId}
+    		});
+    		$.ajax({
+    		    url: baseUrl + "/ehr",
+    		    type: 'POST',
+    		    success: function (data) {
+    		        var ehrId = data.ehrId;
+    		        var partyData = {
+    		            firstNames: ime,
+    		            lastNames: priimek,
+    		            dateOfBirth: datumRojstva,
+    		            partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+    		        };
+    		        $.ajax({
+    		            url: baseUrl + "/demographics/party",
+    		            type: 'POST',
+    		            contentType: 'application/json',
+    		            data: JSON.stringify(partyData),
+    		            success: function (party) {
+    		                if (party.action == 'CREATE') {
+    		                    $('#preberiPredlogoBolnika').append('<option value="'+oseba[0]+","+oseba[1]+","+oseba[2] +","+ehrId+'">'+oseba[0]+" "+oseba[1]+'</option>');
+    		                  
+    		                }
+    		            },
+    		            error: function(err) {
+    		            	$("#kreirajSporocilo").html("<span class='obvestilo label " +
+                        "label-danger fade-in'>Napaka '" +
+                        JSON.parse(err.responseText).userMessage + "'!");
+    		            }
+    		        });
+    		    }
+    		});
+    	}
+}
